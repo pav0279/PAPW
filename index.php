e259570d9e97e38c21783993f5489969a308136e
pav0279 <pavelkova114@gmail.com>
2016-10-12 10:06:33 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -12,12 +12,28 @@
 	   </style>        
 </head>
 <body>
+<form>
+Počet opakování: <input name="pocet" value="3"/> </br>
+Slovo k opakování: <input name="brum" value="Tři" /></p>
+Co bys rád? </br>
+Odrážky?<input type="radio" name="stejne" value="prvni">
+Odstavce? <input type="radio" name="stejne" value="druha">
+<input type = "submit"/>
+</form>
+
+
+
 <?php
-	print "</p><b><center>ččč</center></b></p>";
+	print $_REQUEST["pocet"];
+	print $_REQUEST["brum"];
+	print "</p><b><h1>Výsledek:</h1></b>";
 	print "<ol>";
-	for ($i = 0; $i<10; $i++)  { 
-		print  "<li><b>hhhh".$i."</b></li>" ;
+	for ($i = 0; $i<$_REQUEST["pocet"]; $i++)  { 
+		print  "<li><b>".$i.$_REQUEST["brum"]."</b></li>" ;
+		
 	}
+	
 ?>
 </body>
 </html>
+
