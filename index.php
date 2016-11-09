8703d3ddc72a6aeed022c2220deb4e5805109498
pav0279 <pavelkova114@gmail.com>
2016-11-09 10:28:46 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -9,9 +9,51 @@
 	<meta name="robots" content="index, follow">
 	<meta name="revisit-after" content="7 days">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-	        
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		
+	<title>Quick Start - Leaflet</title>
+
+	<meta charset="utf-8" />
+
+	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.1/dist/leaflet.css" />
+	<script src="https://unpkg.com/leaflet@1.0.1/dist/leaflet.js"></script>
+	<script src="http://gisak.vsb.cz/ruzicka/lib/leaflet/showplace.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script>
+$(document).ready(function(){ //pracuj az je cele html natahle
+    $("#b1").click(function(){ //click po kliknuti mysi, to p je nejaky selector, veme vsechny paragrafy odstavce
+        $(this).hide(); //this - aktualni paragraf
+    });
+
+	$("#hide").click(function(){
+		$("#d1").hide();
+	});
+
+	$("#show").click(function(){
+		$("#d1").show();
+	});
+});
+	$(document).ready(function(){
+		
+		setMap('mapid');
+		$("#mapid").hide();
+		
+		$("strong").click(function(){
+			
+			$("#mapid").show();
+			
+			var lat = $(this).attr("lat");
+			var lon = $(this).attr("lon");
+			var zoom = $(this).attr("zoom");
+			var text = $(this).attr("text");
+			
+			showPlace(lat, lon, zoom, text);
+		});
+	});
+</script>        
 </head>
 <body>
+
 <form>
 	<p>
 	Počet:<input type="number" name="pocet" min="0" max="100" step="1" value="30">
@@ -24,8 +66,104 @@
 	</textarea>
 	<input type="submit"/>	
 	</p>
+
 </form>
+	<div>
+  <h3>This is a heading</h3>
+  <p>This is a paragraph.</p>
+</div>
+
+
 <?php
+	function connect($misto){
+// Let's pass in a $_GET variable to our example, in this case
+// it's aid for actor_id in our Sakila database. Let's make it
+// default to 1, and cast it to an integer as to avoid SQL injection
+// and/or related security problems. Handling all of this goes beyond
+// the scope of this simple example. Example:
+//   http://example.org/script.php?aid=42
+if (isset($_GET['aid']) && is_numeric($_GET['aid'])) {
+    $aid = (int) $_GET['aid'];
+} else {
+    $aid = 1;
+}
+
+// Connecting to and selecting a MySQL database named sakila
+// Hostname: 127.0.0.1, username: your_user, password: your_pass, db: sakila
+$mysqli = new mysqli('127.0.0.1', 'root', 'root', 'pav0279');
+
+// Oh no! A connect_errno exists so the connection attempt failed!
+if ($mysqli->connect_errno) {
+    // The connection failed. What do you want to do? 
+    // You could contact yourself (email?), log the error, show a nice page, etc.
+    // You do not want to reveal sensitive information
+
+    // Let's try this:
+    echo "Sorry, this website is experiencing problems.";
+
+    // Something you should not do on a public site, but this example will show you
+    // anyways, is print out MySQL error related information -- you might log this
+    echo "Error: Failed to make a MySQL connection, here is why: \n";
+    echo "Errno: " . $mysqli->connect_errno . "\n";
+    echo "Error: " . $mysqli->connect_error . "\n";
+    
+    // You might want to show them something nice, but we will simply exit
+    exit;
+}
+
+// Perform an SQL query
+$sql = "SELECT * FROM mista WHERE nazev LIKE '%".$misto."%'";
+if (!$result = $mysqli->query($sql)) { //metoda query
+    // Oh no! The query failed. 
+    echo "Sorry, the website is experiencing problems.";
+
+    // Again, do not do this on a public site, but we'll show you how
+    // to get the error information
+    echo "Error: Our query failed to execute and here is why: \n";
+    echo "Query: " . $sql . "\n";
+    echo "Errno: " . $mysqli->errno . "\n";
+    echo "Error: " . $mysqli->error . "\n";
+    exit;
+}
+
+// Phew, we made it. We know our MySQL connection and query 
+// succeeded, but do we have a result?
+if ($result->num_rows === 0) {
+    // Oh, no rows! Sometimes that's expected and okay, sometimes
+    // it is not. You decide. In this case, maybe actor_id was too
+    // large? 
+    echo "We could not find a match for ID $aid, sorry about that. Please try again.";
+    exit;
+}
+
+// Now, we know only one result will exist in this example so let's 
+// fetch it into an associated array where the array's keys are the 
+// table's column names
+$actor = $result->fetch_assoc();
+echo "Sometimes I see " . $actor['nazev'] . " " . $actor['lat'] . " on TV.";
+
+// Now, let's fetch five random actors and output their names to a list.
+// We'll add less error handling here as you can do that on your own now
+$sql = "SELECT * FROM mista";
+if (!$result = $mysqli->query($sql)) {
+    echo "Sorry, the website is experiencing problems.";
+    exit;
+}
+
+// Print our 5 random actors in a list, and link to each actor
+echo "<ul>\n";
+while ($actor = $result->fetch_assoc()) {
+    echo "<li><a href='" . $_SERVER['SCRIPT_FILENAME'] . "?aid=" . $actor['id'] . "'>\n";
+    echo $actor['lat'] . ' ' . $actor['lon'];
+    echo "</a></li>\n";
+}
+echo "</ul>\n";
+
+// The script will automatically free the result and close the MySQL
+// connection when it exits, but let's just do it anyways
+$result->free();
+$mysqli->close();
+}
 	echo "<h1>Ahoj Karle</h1>";
 	echo "<p>".$_REQUEST["text1"]."</p>";
 	
@@ -46,15 +184,20 @@
 	
 	$ostrava = array("Ostrava", "Ostravě", "Ostravou"); 
 	
+	
+	
 	for ($i=0; $i<count($pieces); $i++) {
 		$word = trim($pieces[$i], ". \t\n");
 		$word = str_replace(".", "", $word);
 		if (in_array($word, $ostrava)) {
 			$pieces[$i] = "<strong>".$pieces[$i]."</strong>";
 		}
+		echo "<div id=\"d1\">".$pieces[$i]."</div>";
 	}
 	$text1_strong_ostrava = implode(" ", $pieces);
 	echo "<p>".$text1_strong_ostrava."</p>";
+	
+
 	/*
 	echo "<ol>";
 	for ($i=1; $i<=$_REQUEST["pocet"]; $i++) {
@@ -66,6 +209,14 @@
 	}
 	echo "</ol>";	
 	*/
+	connect("Brno");
 ?>
+<button id="hide">Hide</button>
+<button id="show">Show</button>
+<p><strong lat="50.505" lon="16.09" zoom="10" text="Zde je <strong>Rtyně</strong>">Rtyně</strong> je divné místo.</p>
+<p>Ale <strong lat="50.505" lon="17.99" zoom="12" text="Zde je <strong>Gogolin</strong>">Gogolin</strong> je ještě divnější.</p>
+<p>Ale <strong lat="49.83483" lon="18.28254" zoom="15" text="Zde je <strong>Ostrava!!!</strong>">Ostrava</strong> je ještě divnější.</p>
+
+<div id="mapid" style="width: 600px; height: 400px;"></div>
 </body>
 </html>
